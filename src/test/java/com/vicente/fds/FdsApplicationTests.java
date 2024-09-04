723d4a1b661a97fed8cec5c8bd65dffe48e70820
Vicente Piltcher <vipiltcher62@gmail.com>
2024-09-04 18:13:30 -0300

    first commit

---
+++ src/test/java/com/vicente/fds/FdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicente.fds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
